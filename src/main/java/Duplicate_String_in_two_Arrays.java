e792a88687d08fcbeb26ac4fc25622f262de0ba6
desterousDangi <inboxpardeepdangi@gmail.com>
2022-04-07 23:48:53 +0530

    comment modified

---
+++ src/main/java/Duplicate_String_in_two_Arrays.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Duplicate_String_in_two_Arrays {
+
+	public static void main(String[] args) {
+		String[] array1 = { "chiku", "aman", "rohan", "ram", "mohan", "ali" };
+
+		String[] array2 = { "rohan", "Aman", "Ali", "dangi", "pinku", "Mohan"  };
+
+
+		System.out.println("Array1 : " + Arrays.toString(array1));
+		System.out.println("Array2 : " + Arrays.toString(array2));
+
+		HashSet<String> set = new HashSet<String>();
+
+		for (int i = 0; i < array1.length; i++) {
+			for (int j = 0; j < array2.length; j++) {
+				if (array1[i].equals(array2[j])) {
+					set.add(array1[i]);
+				}
+				if (array1[i].equalsIgnoreCase(array2[j])) {
+					System.out.println(array1[i] + " is dulpicat of " + array2[j]);
+					//System.out.println("the difference is only of cases");
+				}
+			}
+		}
+
+		System.out.println("Common element : " + (set)); 
+	}
+}
