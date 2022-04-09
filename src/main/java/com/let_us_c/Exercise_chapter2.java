7f84a4d578552ca59ab2c212760d7a8d100bc65b
desterousDangi <inboxpardeepdangi@gmail.com>
2022-04-09 22:25:37 +0530

    basic exercise

---
+++ src/main/java/com/let_us_c/Exercise_chapter2.java
@@ -0,0 +1,17 @@
+package com.let_us_c;
+
+public class Exercise_chapter2 {
+
+	public static void main(String[] args) {	
+		
+		float a = 12.25f, b = 12.52f ;
+		if ( a == b )
+		System.out.println(	 "\na and b are equal" ) ; 
+		
+		System.out.println( a<b);
+		System.out.println( a!=b);
+		System.out.println( a=243.5f);
+		
+	}
+
+}
