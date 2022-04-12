aa3a1fc969da29462ff60e7ce35c030c7260900c
desterousDangi <inboxpardeepdangi@gmail.com>
2022-04-13 01:27:44 +0530

    code updated

--- src/main/java/Array_to_arrayList.java
+++ src/main/java/Array_to_arrayList.java
@@ -8,13 +8,21 @@ public class Array_to_arrayList {
 	public static void main(String[] args) {
 		  String[]  my_array = new String[] {"Python", "JAVA", "PHP",  "Perl", "C#", "C++"};
 		  System.out.println(Arrays.toString(my_array));
-		  
+		  //converting array into the list
 		  List<String> al	= Arrays.asList(my_array);
 		  ArrayList<String>  list = new ArrayList<String>(al);
 		  
 		  System.out.println(list);
 		  Collections.sort(list);
 		  System.out.println(list);
+		  
+		  // converting list into array
+		  String [] converted_array = new String[list.size()];
+		  converted_array =  list.toArray(converted_array);
+		  for(String ss: converted_array) {
+			  System.out.println(ss);
+		  }
+		  System.out.println(Arrays.toString(converted_array));
 	}
 
 }
