317a72f25c3fc389b54013e07da4b09d4de84893
desterousDangi <inboxpardeepdangi@gmail.com>
2022-04-12 21:17:05 +0530

    violating singleton class rules using Cloneable interface

--- src/main/java/Singleton.java
+++ src/main/java/Singleton.java
@@ -1,5 +1,5 @@
 
-public class Singleton {
+public class Singleton implements Cloneable {
      
 	private static final Singleton soleInstance =  new Singleton();
 
@@ -14,18 +14,33 @@ public class Singleton {
 	public static Singleton getIntsance() { 
 		return soleInstance;    // sole instance available
 	}
+	protected Object clone() throws  CloneNotSupportedException {
+		return super.clone();
+		
+	}
 }
 
 
 class Testclass {
-	public static void main(String []args) {
+	public static void main(String []args) throws CloneNotSupportedException {
 		Singleton s1= Singleton.getIntsance();
 		Singleton s2= Singleton.getIntsance();
 		
-		// check out their Hashcode if same then they are pointing to same reference
+		// check out their hashcode if same then they are pointing to same reference
 		print("s1", s1);
 		print("s2", s2);
 		
+		// requirement of single instance can be violated using:
+		/*
+		 * cloneable
+		 * serializable/deserialization
+		 * reflection
+		 * multithreading access
+		 * multiple class loaders
+		 * Garbage collection
+		 */
+		Singleton s3 = (Singleton) s2.clone();
+		print("s3",s3);
 	}
 
     static void print(String string, Singleton object) {
@@ -33,8 +48,3 @@ class Testclass {
     	System.out.println(String.format("Object : %s, Hashcode: %d ", string, object.hashCode()));
 	}
 }
-/*
-creating ...
-Object : s1, Hashcode: 366712642 
-Object : s2, Hashcode: 366712642 
-*/
