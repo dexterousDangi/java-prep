1dc8975baceac0ccf537d45e1cdb8cf84b39a490
desterousDangi <inboxpardeepdangi@gmail.com>
2022-04-16 22:32:32 +0530

    code added

--- src/main/java/String_Palindrome.java
+++ src/main/java/String_Palindrome.java
@@ -20,6 +20,30 @@ public class String_Palindrome {
 			System.out.println("string or number enter is palindrome");
 		else
 			System.out.println("not a palindrome");
+		
+		System.out.println("again checking if there is some upper case or lower case problem");
+		
+		if(str.equalsIgnoreCase(str1))
+			System.out.println(" is a palindorme and cases are ignored");
+		else
+			System.out.println("not a palindrome");
+		// another way to check  the palindrome
+		checkPalindrome(str);
+	}
+
+	private static void checkPalindrome(String str) {
+	    String string =str;
+	    String strg ="";
+	    char c ;
+	    for(int i=string.length()-1;i>=0;i--) {
+	    	c= string.charAt(i);
+	    	strg =strg +c;
+	    }
+	    System.out.println("againg a new way to reverse a string \n "+ strg);
+	if(string.equals(strg)!=false)
+		System.out.println("it is a palindrome");
+	else
+		System.out.println("not a palindrome");
 	}
 
 }
