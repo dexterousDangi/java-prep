40d2ce88b1158d5dc0a04bd4252db7ce7b92245a
desterousDangi <inboxpardeepdangi@gmail.com>
2022-04-21 23:19:40 +0530

    About Daemon Thread

---
+++ src/main/java/Multithreading_Daemon_thread.java
@@ -0,0 +1,39 @@
+// Daemon thread in Java is a low-priority thread that runs in the background to perform tasks such as garbage collection. 
+// if the task complets before the execution of deamon thread exection finishes, then the remaining part will not execute and 
+// if main thread or the normal thread take long time enough then both the thread will be executed i.e., main thread and the daemon thread
+ 
+// we have to set a normal thread as Daemon thread using setDaemon(boolean) method of thread class.
+
+public class Multithreading_Daemon_thread {
+
+	public static void main(String[] args) {
+		Daemon_thread dt = new Daemon_thread();
+		dt.setDaemon(true);
+		
+		for(int i=1;i<3; i++) {
+			System.out.println(Thread.currentThread().getName() + " "+ Thread.currentThread().getId());
+		}
+		dt.start();
+	}
+
+}
+class Daemon_thread extends Thread{
+	public void run() { 
+		for(int i=1; i<=10; i++) {
+			System.out.println(Thread.currentThread().getName()+" "+Thread.currentThread().isDaemon());
+			
+		}
+	}
+}
+
+/*output:
+main 1
+main 1
+Thread-0 true
+Thread-0 true
+Thread-0 true
+Thread-0 true
+Thread-0 true
+Thread-0 true
+Thread-0 true
+*/
