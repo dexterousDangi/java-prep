7076ddd639235e6fec2d6aa14ab2f41ea8a98476
desterousDangi <inboxpardeepdangi@gmail.com>
2022-04-28 23:41:19 +0530

    code added

--- src/main/java/Multithreading_using_anonymous_class.java
+++ src/main/java/Multithreading_using_anonymous_class.java
@@ -3,16 +3,23 @@
 // to create the another thread we have to write another anonymous class.
 
 public class Multithreading_using_anonymous_class {
+	Thread t3 = new Thread() {
+		public void run() {
+			System.out.println(Thread.currentThread().getName());
+			Thread.currentThread().setName("anonymous thread 3");
+			System.out.println(Thread.currentThread().getName());
+		}
+	};
 
 	public static void main(String[] args) {
-		Thread t1=new Thread() {
+		Thread t1 = new Thread() {
 			public void run() {
 				System.out.println(Thread.currentThread().getName());
 				Thread.currentThread().setName("anonymous thread 1");
 				System.out.println(Thread.currentThread().getName());
 			}
 		};
-		Thread t2=new Thread() {
+		Thread t2 = new Thread() {
 			public void run() {
 				System.out.println(Thread.currentThread().getName());
 				Thread.currentThread().setName("anonymous thread 2");
@@ -20,8 +27,13 @@ public class Multithreading_using_anonymous_class {
 			}
 		};
 		t1.start();
-				t2.start();
-				System.out.println("total active thread at the given time: "+Thread.activeCount());
+		t2.start();
+
+		Multithreading_using_anonymous_class mb = new Multithreading_using_anonymous_class();
+		
+		mb.t3.start();   // when thread is created in instance area not in the main method 
+
+		System.out.println("total active thread at the given time: " + Thread.activeCount());
 		System.out.println(Thread.currentThread().getName());
 	}
 
