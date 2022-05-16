df39c2364c320a95fc067706f3b92cd79a077720
desterousDangi <inboxpardeepdangi@gmail.com>
2022-05-16 05:33:07 +0530

    Selection sort

---
+++ src/main/java/Interview/question/Selection_sort.java
@@ -0,0 +1,29 @@
+package Interview.question;
+
+import java.util.Arrays;
+
+// selection_sort: time complexity of this algo is O(n^2);
+// 
+
+public class Selection_sort {
+	
+	public static void main(String[] args) {
+		int b[] = { 44, 66, 99, 77, 33, 22, 55 };
+		int temp = 0;
+		for (int i = 0; i < b.length; i++) {
+			int min = i;
+			for (int j = i + 1; j < b.length; j++) {
+
+				if (b[min] > b[j]) {
+					min = j;
+				}
+			}
+			temp = b[i];
+			b[i] = b[min];
+			b[min] = temp;
+
+		}
+		System.out.println(Arrays.toString(b));
+	}
+
+}
