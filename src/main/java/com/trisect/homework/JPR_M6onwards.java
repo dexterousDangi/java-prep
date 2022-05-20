0cf3586302b952a00073fc2d44a512fc6ad4f7df
desterousDangi <inboxpardeepdangi@gmail.com>
2022-05-20 22:33:33 +0530

    trisect M serise M6 finished

--- src/main/java/com/trisect/homework/JPR_M6onwards.java
+++ src/main/java/com/trisect/homework/JPR_M6onwards.java
@@ -46,7 +46,7 @@ public class JPR_M6onwards {
 		System.out.println();
 
 		// task22
-		int n22 = 8;
+		int n22 = 5;
 		for (int i = n22; i >= 1; i--) {
 			for (int j = i; j >= 1; j--) {
 				if (j == 1) {
@@ -88,113 +88,229 @@ public class JPR_M6onwards {
 				System.out.print("*");
 			}
 			System.out.println();
-		}System.out.println();
-		
+		}
+		System.out.println();
+
 		// task24
-		int n24=6;
-		for(int i=1;i<=n24;i++) {
-			for(int k=1;k<i;k++) {
+		int n24 = 6;
+		for (int i = 1; i <= n24; i++) {
+			for (int k = 1; k < i; k++) {
 				System.out.print("*");
 			}
-			for(int j=i;j<=n24;j++) {
-				if(j==i) {
+			for (int j = i; j <= n24; j++) {
+				if (j == i) {
 					System.out.print(j);
-				}else {
-					System.out.print("*"+j);
+				} else {
+					System.out.print("*" + j);
 				}
-			}for(int k=1;k<i;k++) {
+			}
+			for (int k = 1; k < i; k++) {
 				System.out.print("*");
 			}
-			System.out.println();
-		}System.out.println();
-		
-		// task25
-		int n25=5;
-		for(int i=1;i<=n25;i++) {
-			for(int j=i;j<n25;j++) {
-				System.out.print("*");
-			}
-			for(int k=1;k<=i;k++) {
-				if(k==1) {
-					System.out.print(i);
-				}else {
-					System.out.print("*"+i);
-				}
-			}for(int j=i;j<n25;j++) {
-				System.out.print("*");
-			}
-			
-			System.out.println();
-		}System.out.println();
-		
-		//task26
-		int n26=5;
-		for(int i=1;i<=n26;i++) {
-			for(int j=i;j<n26;j++) {
-				System.out.print("*");
-			}
-			for(int k=1;k<=i;k++) {
-				System.out.print(k);
-			}
-			for(int l=i-1;l>=1;l--) {
-				System.out.print(l);
-			}
-			for(int j=i;j<n26;j++) {
-				System.out.print("*");
-			}
-			
 			System.out.println();
 		}
 		System.out.println();
-		
-		//task27
-		int n27=6;
-		for(int i=1;i<=n27;i++) {
-			for(int k=i;k>1;k--) {
+
+		// task25
+		int n25 = 5;
+		for (int i = 1; i <= n25; i++) {
+			for (int j = i; j < n25; j++) {
 				System.out.print("*");
 			}
-			for(int j=i;j<=n27;j++) {
-				if(j==i) {
-					System.out.print(j);
-				}else {
-					System.out.print("*"+j);
+			for (int k = 1; k <= i; k++) {
+				if (k == 1) {
+					System.out.print(i);
+				} else {
+					System.out.print("*" + i);
 				}
-			}for(int k=i;k>1;k--) {
+			}
+			for (int j = i; j < n25; j++) {
 				System.out.print("*");
-			}			
+			}
+
 			System.out.println();
 		}
-		for(int i=n27-1;i>=1;i--) {
-			for(int k=i;k>1;k--) {
+		System.out.println();
+
+		// task26
+		int n26 = 5;
+		for (int i = 1; i <= n26; i++) {
+			for (int j = i; j < n26; j++) {
 				System.out.print("*");
-			}			
-			for(int j=i;j<=n27;j++) {
-				if(j==i) {
+			}
+			for (int k = 1; k <= i; k++) {
+				System.out.print(k);
+			}
+			for (int l = i - 1; l >= 1; l--) {
+				System.out.print(l);
+			}
+			for (int j = i; j < n26; j++) {
+				System.out.print("*");
+			}
+
+			System.out.println();
+		}
+		System.out.println();
+
+		// task27
+		int n27 = 6;
+		for (int i = 1; i <= n27; i++) {
+			for (int k = i; k > 1; k--) {
+				System.out.print("*");
+			}
+			for (int j = i; j <= n27; j++) {
+				if (j == i) {
 					System.out.print(j);
-				}else {
-					System.out.print("*"+j);
+				} else {
+					System.out.print("*" + j);
 				}
-			}for(int k=i;k>1;k--) {
+			}
+			for (int k = i; k > 1; k--) {
 				System.out.print("*");
-			}				
+			}
+			System.out.println();
+		}
+		for (int i = n27 - 1; i >= 1; i--) {
+			for (int k = i; k > 1; k--) {
+				System.out.print("*");
+			}
+			for (int j = i; j <= n27; j++) {
+				if (j == i) {
+					System.out.print(j);
+				} else {
+					System.out.print("*" + j);
+				}
+			}
+			for (int k = i; k > 1; k--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		// task 29
+
+		int n29 = 5;
+		for (int i = n29; i >= 1; i--) {
+			for (int j = n29 - i; j >= 1; j--) {
+				System.out.print("*");
+			}
+			for (int k = 1; k <= i; k++) {
+				System.out.print(k);
+			}
+			for (int m = (i - 1); m >= 1; m--) {
+				System.out.print(m);
+			}
+			for (int j = n29 - i; j >= 1; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		// task30
+		int n30 = 7;
+		for (int i = 1; i <= n30; i++) {
+			for (int k = n30; k > i; k--) {
+				System.out.print("*");
+			}
+			for (int j = 1; j < 2 * i; j++) {
+				System.out.print("1");
+			}
+			for (int k = n30; k > i; k--) {
+				System.out.print("*");
+			}
+			System.out.println();
+
+		}
+		for (int i = n30 - 1; i >= 1; i--) {
+			for (int j = 1; j <= n30 - i; j++) {
+				System.out.print("*");
+			}
+			for (int k = 1; k < 2 * i; k++) {
+				System.out.print("1");
+			}
+			for (int j = 1; j <= n30 - i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		// task31
+		int n31 = 4;
+		for (int i = 1; i <= n31; i++) {
+			for (int j = i; j < n31; j++) {
+				System.out.print("*");
+			}
+			for (int k = i; k >= 1; k--) {
+				if (k == i) {
+					System.out.print(i);
+				} else {
+					System.out.print("*" + i);
+				}
+			}
+			for (int j = i; j < n31; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (int i = n31 - 1; i >= 1; i--) {
+			for (int k = i; k < n31; k++) {
+				System.out.print("*");
+			}
+			for (int j = i; j >= 1; j--) {
+				if (j == i) {
+					System.out.print(i);
+				} else {
+					System.out.print("*" + i);
+				}
+			}
+			for (int k = i; k < n31; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		// task 32
+
+		int n32 = 5;
+		int count = 0;
+		for (int i = 1; i <= n32; i++) {
+			for (int j = 1; j <= i; j++) {
+				count = count + 1;
+				System.out.print(count + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		// task 33 
+		int n33 = 5;
+		int count1 = 0;
+		for (int i = 1; i <= n33; i++) {
+			count1 = i;
+			for (int j = 1; j<=i ; j++) {
+				System.out.print(count1+" ");
+				count1 = count1 + (n33 - j);
+
+			}
 			System.out.println();
 		}System.out.println();
-		
-		//task 29
-		
-		
+
+		// task 34
+		int n34 = 5;
+		char c;
+
+		for (int i = 1; i <= n34; i++) {
+			c = 64;
+			c = (char) (c + i);
+			for (int j = 1; j <= i; j++) {
+
+				System.out.print(c);
+			}
+			System.out.println();
+		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
