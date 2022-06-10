f736796e390737a58ba747bed32a5f462d0e66c0
desterousDangi <inboxpardeepdangi@gmail.com>
2022-06-10 19:48:01 +0530

    Equal and Double equal opeator usage in Strings

---
+++ src/main/java/StringEqualAndDoubleEqualOperatior.java
@@ -0,0 +1,21 @@
+
+public class StringEqualAndDoubleEqualOperatior {
+
+	// about string manipulation
+	// difference between string and stringbuffer class
+
+	
+	public static void main(String []args)
+
+	{
+
+	String s1= "Dangi1";
+
+	String s2 = new String ("Dangi2");
+
+	System.out.println(s1);
+
+	System.out.println(s2);
+	}
+}
+
