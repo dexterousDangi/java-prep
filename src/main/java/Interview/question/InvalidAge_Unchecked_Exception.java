cded1ee89013d652f01fe8f48b9b74af518c411c
desterousDangi <inboxpardeepdangi@gmail.com>
2022-06-19 09:07:16 +0530

    userdefined runtime exception creation

---
+++ src/main/java/Interview/question/InvalidAge_Unchecked_Exception.java
@@ -0,0 +1,12 @@
+package Interview.question;
+
+@SuppressWarnings("serial")
+public class InvalidAge_Unchecked_Exception extends RuntimeException {
+
+	InvalidAge_Unchecked_Exception(){
+		
+	}
+	InvalidAge_Unchecked_Exception(String str){
+		super(str);
+	}
+}
