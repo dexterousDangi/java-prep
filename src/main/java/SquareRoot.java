221a441e0559ee31ac792bc97028b6af7d313a57
desterousDangi <inboxpardeepdangi@gmail.com>
2022-06-22 23:15:20 +0530

    finding square root using Math.sqrt() method

---
+++ src/main/java/SquareRoot.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SquareRoot {
+
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+
+			System.out.println("Input a number to find square root: ");
+
+			double square = sc.nextDouble();
+
+			double squareRoot = Math.sqrt(square);
+
+			System.out.printf("The square root is: %f ", squareRoot);
+
+			}
+	}
+
+}
