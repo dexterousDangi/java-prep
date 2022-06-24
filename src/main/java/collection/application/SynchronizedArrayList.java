ddc870cd4c006011c915c079c039041f16f37bbe
desterousDangi <inboxpardeepdangi@gmail.com>
2022-06-24 21:26:08 +0530

    Synchronized ArrayList in java

---
+++ src/main/java/collection/application/SynchronizedArrayList.java
@@ -0,0 +1,32 @@
+package collection.application;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Using Collections.synchronizedList()
+ */
+public class SynchronizedArrayList {
+
+	public static void main(String[] args) {
+
+		  // Converting ArrayList to Synchronized ArrayList
+	       List<String> synchronizedarraylist = 
+	       Collections.synchronizedList(new ArrayList<String>());
+
+	       //Adding elements to synchronized ArrayList
+	       synchronizedarraylist.add("Basketball");
+	       synchronizedarraylist.add("Baseball");
+	       synchronizedarraylist.add("Football");
+	       
+	       System.out.println("Synchronized ArrayList Iteration:");
+	       synchronized(synchronizedarraylist) {
+	       Iterator<String> iterator = synchronizedarraylist.iterator(); 
+	       while (iterator.hasNext())
+	          System.out.println(iterator.next());
+	       }	       
+	}
+
+}
