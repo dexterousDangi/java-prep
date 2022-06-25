5809dde29c7209f64a9984cdee3c449cb6dd248f
desterousDangi <inboxpardeepdangi@gmail.com>
2022-06-25 22:56:48 +0530

    Multithreading wait/notify/notifyAll usage

--- src/main/java/multithreading_wati_notify_notifyAll/Message.java
+++ src/main/java/multithreading_wati_notify_notifyAll/Message.java
@@ -2,17 +2,16 @@ package multithreading_wati_notify_notifyAll;
 
 public class Message {
 	private String msg;
+
 	public Message(String msg) {
-		this.msg=msg;
+		this.msg = msg;
 	}
+
 	public String getMsg() {
 		return msg;
 	}
 
 	public void setMsg(String msg) {
 		this.msg = msg;
-	} 
+	}
 }
-
-	
-
