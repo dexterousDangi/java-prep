61b686b34bff8d07b1af6867ae49499167ce8c06
desterousDangi <inboxpardeepdangi@gmail.com>
2022-07-05 22:30:32 +0530

    Longest Consecutive Sequence In an Array - code modified

--- src/main/java/Interview/question/LongestConsecutiveSequenceInArray.java
+++ src/main/java/Interview/question/LongestConsecutiveSequenceInArray.java
@@ -4,47 +4,44 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class LongestConsecutiveSequenceInArray {
-	
+
 	private static int longestConsecutive(int[] num) {
-		if(num.length==0) {
+		if (num.length == 0) {
 			return 0;
 		}
-		
-		Set<Integer> set =new HashSet<Integer>();
-		int max=1;
-		
-		for(int e:num)
+
+		Set<Integer> set = new HashSet<Integer>();
+		int max = 1;
+
+		for (int e : num)
 			set.add(e);
-		
-		for(int e:num) {
-			int left=e-1;
-			int right=e+1;
-			 
-			int count=1;
-			while(set.contains(left)) {
+
+		for (int e : num) {
+			int left = e - 1;
+			int right = e + 1;
+
+			int count = 1;
+			while (set.contains(left)) {
 				count++;
 				set.remove(left);
 				left--;
 			}
-			
-			while(set.contains(right)) {
+
+			while (set.contains(right)) {
 				count++;
 				set.remove(right);
 				right++;
 			}
-			max=Math.max(count, max);
+			max = Math.max(count, max);
 		}
-		
-		
 		
 		return max;
 	}
 
 	public static void main(String[] args) {
-		int [ ]arr= {121,1,3,2,4,5,25};
+		int[] arr = { 121, 1, 3, 2, 4, 5, 25 };
 		System.out.println(longestConsecutive(arr));
-		
-	}
 
+	}
 
 }
