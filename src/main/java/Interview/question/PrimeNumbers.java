deefb3b347ced61231c6d20bd1d36c30c1078527
desterousDangi <inboxpardeepdangi@gmail.com>
2022-07-15 22:15:39 +0530

    finding primes number between a range given

---
+++ src/main/java/Interview/question/PrimeNumbers.java
@@ -0,0 +1,32 @@
+package Interview.question;
+
+import java.util.ArrayList;
+
+public class PrimeNumbers {
+
+	public static void main(String[] args) {
+		System.out.println(findPrime(1,100));
+	}
+
+	private static ArrayList<Integer> findPrime(int start, int end) {
+		ArrayList<Integer> primes=new ArrayList<>();
+		for(int n=start;n<end;n++) {
+			boolean prime=true;
+			int i=2;
+			while(i<= n/2) {
+				if(n%i==0) {
+					prime=false;
+					break;
+				}
+				i++;
+			}
+			if(prime) {
+				primes.add(n);
+			}
+			
+			
+		}
+		
+		return primes;
+}
+}
