b20fa319400982462554bf4dda492f4c71ae553a
desterousDangi <inboxpardeepdangi@gmail.com>
2022-07-18 23:48:48 +0530

    Added Interface StudentDao

--- src/main/java/dao_Factory_design/StudentDao.java
+++ src/main/java/dao_Factory_design/StudentDao.java
@@ -1,5 +1,10 @@
 package dao_Factory_design;
 
-public interface StudentDao {
+import java.util.List;
 
+public interface StudentDao {
+	   public List<Student> getAllStudents();
+	   public Student getStudent(int rollNo);
+	   public void updateStudent(Student student);
+	   public void deleteStudent(Student student);
 }
